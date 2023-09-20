2451298757da12307137e9e2f0ba97011a646d85
ArtemBudnyk <qrtleo@gmail.com>
2023-09-20 15:06:16 +0300

    new buildings title done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,12 +1,10 @@
-// var navbarInfo = document.querySelector('.description-info')
 
-// const { off } = require("gulp")
 
 var navbarLogo = document.querySelector('.logo')
 
 var toggleButton = document.querySelector('.navbar-toggler')
 
-// var closeButton = document.querySelector('.btn-close ')
+
 
 toggleButton.addEventListener('click', function() {
     navbarLogo.classList.add('width')
@@ -35,30 +33,6 @@ offCanvasCloseEI.addEventListener("click", function () {
 
 
 
-
-// closeButton.addEventListener('click', function() {
-//     navbarLogo.classList.remove('width')
-// })
-
-
-
-
-// toggleButtonAdd.addEventListener('click', function() {
-//     navbarLogo.classList.add('width')
-// })
-
-
-// toggleButton.addEventListener('click', function() {
-//     console.log(toggleButton)
-
-//     var hasChangedWidth = navbarLogo.classList.contains('width')
-
-//     if (hasChangedWidth) {
-//         navbarLogo.classList.remove('width')
-//     } else {
-//         navbarLogo.classList.add('width')
-//     }
-// })
 
 
 
